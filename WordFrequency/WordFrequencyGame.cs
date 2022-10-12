6452196d226f9aa7b2f4673cad7acbd21c9fc405
shiwei.fu <shiwei.fu@thoughtworks.com>
2022-10-12 08:29:14 +0800

    refactor: [Long Function] : [Extract Function]

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -17,38 +17,57 @@ namespace WordFrequency
                 //split the input string with 1 to n pieces of spaces
                 string[] arr = Regex.Split(inputStr, @"\s+");
 
-                List<Input> inputList = new List<Input>();
-                foreach (var s in arr)
-                {
-                    Input input = new Input(s, 1);
-                    inputList.Add(input);
-                }
+                var inputList = GetInputList(arr);
 
                 //get the map for the next step of sizing the same word
-                Dictionary<string, List<Input>> map = GetListMap(inputList);
+                var map = GetListMap(inputList);
 
-                List<Input> list = new List<Input>();
-                foreach (var entry in map)
-                {
-                    Input input = new Input(entry.Key, entry.Value.Count);
-                    list.Add(input);
-                }
+                var noDuplicateInputList = GetNoDuplicateInputs(map);
 
-                inputList = list;
+                noDuplicateInputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
 
-                inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
-
-                List<string> strList = new List<string>();
-
-                //stringJoiner joiner = new stringJoiner("\n");
-                foreach (Input w in inputList)
-                {
-                    string s = w.Value + " " + w.WordCount;
-                    strList.Add(s);
-                }
+                var strList = FromatOutput(noDuplicateInputList);
 
                 return string.Join("\n", strList.ToArray());
             }
+        }
+
+        private static List<string> FromatOutput(List<Input> noDuplicateInputList)
+        {
+            List<string> strList = new List<string>();
+
+            //stringJoiner joiner = new stringJoiner("\n");
+            foreach (Input w in noDuplicateInputList)
+            {
+                string s = w.Value + " " + w.WordCount;
+                strList.Add(s);
+            }
+
+            return strList;
+        }
+
+        private static List<Input> GetNoDuplicateInputs(Dictionary<string, List<Input>> map)
+        {
+            List<Input> list = new List<Input>();
+            foreach (var entry in map)
+            {
+                Input input = new Input(entry.Key, entry.Value.Count);
+                list.Add(input);
+            }
+
+            return list;
+        }
+
+        private static List<Input> GetInputList(string[] arr)
+        {
+            List<Input> inputList = new List<Input>();
+            foreach (var s in arr)
+            {
+                Input input = new Input(s, 1);
+                inputList.Add(input);
+            }
+
+            return inputList;
         }
 
         private Dictionary<string, List<Input>> GetListMap(List<Input> inputList)
