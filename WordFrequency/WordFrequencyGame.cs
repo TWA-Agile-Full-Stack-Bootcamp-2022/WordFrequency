14a932e0db7eec40cce21f928bb2bef37f316b75
Yixi <lyh1112@gmail.com>
2023-10-17 10:51:10 +0800

    refactor: Complex data structure : calculate string count use stringp[] directly

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace WordFrequency
 {
@@ -17,59 +18,25 @@ namespace WordFrequency
                 //split the input string with 1 to n pieces of spaces
                 string[] words = Regex.Split(inputStr, @"\s+");
 
-                List<Input> inputList = new List<Input>();
-                foreach (var s in words)
+                Dictionary<string, int> wordFrequencyMap = new Dictionary<string, int>();
+
+                foreach (var word in words)
                 {
-                    Input input = new Input(s, 1);
-                    inputList.Add(input);
+                    if (!wordFrequencyMap.ContainsKey(word))
+                    {
+                        wordFrequencyMap.Add(word, 1);
+                    }
+                    else
+                    {
+                        wordFrequencyMap[word]++;
+                    }
                 }
 
-                //get the map for the next step of sizing the same word
-                Dictionary<string, List<Input>> map = GetListMap(inputList);
+                var sortedWordFrequency = wordFrequencyMap.OrderByDescending(x => x.Value)
+                .Select(x => $"{x.Key} {x.Value}").ToArray();
 
-                List<Input> list = new List<Input>();
-                foreach (var entry in map)
-                {
-                    Input input = new Input(entry.Key, entry.Value.Count);
-                    list.Add(input);
-                }
-
-                inputList = list;
-
-                inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
-
-                List<string> strList = new List<string>();
-
-                //stringJoiner joiner = new stringJoiner("\n");
-                foreach (Input w in inputList)
-                {
-                    string s = w.Value + " " + w.WordCount;
-                    strList.Add(s);
-                }
-
-                return string.Join("\n", strList.ToArray());
+                return string.Join("\n", sortedWordFrequency);
             }
-        }
-
-        private Dictionary<string, List<Input>> GetListMap(List<Input> inputList)
-        {
-            Dictionary<string, List<Input>> map = new Dictionary<string, List<Input>>();
-            foreach (var input in inputList)
-            {
-                //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-                if (!map.ContainsKey(input.Value))
-                {
-                    List<Input> arr = new List<Input>();
-                    arr.Add(input);
-                    map.Add(input.Value, arr);
-                }
-                else
-                {
-                    map[input.Value].Add(input);
-                }
-            }
-
-            return map;
         }
     }
 }
