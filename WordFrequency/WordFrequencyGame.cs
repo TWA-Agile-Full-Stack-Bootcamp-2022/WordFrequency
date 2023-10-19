ed260a1f48848b04d81f9b1db4cb1bd9c2cf6389
yoogodong <yoogdong@gmail.com>
2023-10-19 19:23:55 +0800

    refactor: extract method

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -10,64 +10,36 @@ namespace WordFrequency
 
         public string CountWords(string inputStr)
         {
-            if (Regex.Split(inputStr, SPACE).Length == 1)
-            {
-                return inputStr + " 1";
-            }
-            else
-            {
-                //split the input string with 1 to n pieces of spaces
-                string[] words = Regex.Split(inputStr, SPACE);
-
-                List<WordCount> originWordCountList = new List<WordCount>();
-                foreach (var word in words)
-                {
-                    WordCount input = new WordCount(word, 1);
-                    originWordCountList.Add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Dictionary<string, List<WordCount>> word2WordCountList = GetListMap(originWordCountList);
-
-                List<WordCount> resultWordCountList = new List<WordCount>();
-                foreach (var entry in word2WordCountList)
-                {
-                    WordCount input = new WordCount(entry.Key, entry.Value.Count);
-                    resultWordCountList.Add(input);
-                }
-
-                resultWordCountList.Sort((w1, w2) => w2.Count - w1.Count);
-
-                List<string> strList = new List<string>();
-
-                foreach (WordCount w in resultWordCountList)
-                {
-                    string s = w.Word + " " + w.Count;
-                    strList.Add(s);
-                }
-
-                return string.Join("\n", strList.ToArray());
-            }
+            List<WordCount> resultWordCountList = ToWordCountList(inputStr);
+            resultWordCountList.Sort((w1, w2) => w2.Count - w1.Count);
+            List<string> strList = new List<string>();
+            resultWordCountList.ForEach(w => strList.Add(w.Word + " " + w.Count));
+            return string.Join("\n", strList.ToArray());
         }
 
-        private Dictionary<string, List<WordCount>> GetListMap(List<WordCount> wordCountList)
+        private List<WordCount> ToWordCountList(string inputStr)
         {
-            Dictionary<string, List<WordCount>> map = new Dictionary<string, List<WordCount>>();
-            foreach (var wordCount in wordCountList)
+            string[] words = Regex.Split(inputStr, SPACE);
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            foreach (var word in words)
             {
-                if (!map.ContainsKey(wordCount.Word))
+                if (dictionary.TryGetValue(word, out int count))
                 {
-                    List<WordCount> arr = new List<WordCount>();
-                    arr.Add(wordCount);
-                    map.Add(wordCount.Word, arr);
+                    dictionary[word] = count+1;
                 }
                 else
                 {
-                    map[wordCount.Word].Add(wordCount);
+                    dictionary.Add(word, 1);
                 }
             }
 
-            return map;
+            List<WordCount> wordCounts = new List<WordCount>();
+            foreach (var entry in dictionary)
+            {
+                wordCounts.Add(new WordCount(entry.Key, entry.Value));
+            }
+
+            return wordCounts;
         }
     }
 }
